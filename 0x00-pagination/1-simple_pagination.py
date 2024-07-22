ee9f9eb4100df84f5f04ee9a6ae586ae6815e8ae
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-22 11:04:48 +0200

    Method takes two integer arguments page with default value 1 and page_size with default value 10.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -12,12 +12,16 @@ from typing import List
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
+    Takes two integer arguments page and page_size
+
     Parameters
         page: The page number
         page_size: The number of items per page
 
     Return
-        A tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters
+        A tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters
     """
 
     return ((page - 1) * page_size, page * page_size)
@@ -48,12 +52,16 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
+        Takes two integer arguments page with default value 1 and page_size
+        with default value 10
+
         Parameters
             page: The page number
             page_size: The number of items per page
 
         Return
-            A list of lists of strings, where each list represents a row in the dataset
+            A list of lists of strings, where each list represents a row in the
+            dataset
         """
 
         assert isinstance(page, int) and page > 0

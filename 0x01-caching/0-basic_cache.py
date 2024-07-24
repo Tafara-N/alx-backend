c6ead5b1570ffad03d655a4fee22331e11c31ee4
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-24 13:51:35 +0200

    A class BasicCache that inherits from BaseCaching and is a caching system.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+A class `BasicCache` that inherits from `BaseCaching` and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A class `BasicCache` that inherits from `BaseCaching` and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assign to the dictionary `cache_data` the `item` value for the key `key`
+        """
+
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value in `cache_data` linked to `key` key
+        """
+        
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

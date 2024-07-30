fe3f2609d8ded7bb97d62d303acbe088e56c690a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-30 18:02:02 +0200

    A Config class that has a LANGUAGES class attribute.

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Flask: Basic Babel setup
+"""
+
+from config import Config
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """
+    The index page route, renders the '1-index.html' template
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()

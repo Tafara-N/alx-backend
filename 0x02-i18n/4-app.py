98d758f33fdc8ec97d12f228ca9819e7f9de37c3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-30 18:40:36 +0200

    Implementing a way to force a particular locale by passing the 'locale=fr' parameter to my app’s URLs.

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+Implementing a way to force a particular locale by passing the 'locale=fr'
+parameter to my app's URLs
+"""
+
+from typing import Union
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from config import Config
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """
+    Gets locale from a request
+    """
+
+    locale = request.args.get("locale")
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home():
+    """
+    The index page route, renders the '4-index.html' template
+    """
+
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run()

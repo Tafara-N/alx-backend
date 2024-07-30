2064a184a292da3ce96a211ef4ea419e64d1fb1b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-30 18:10:15 +0200

    Function to determine the best match with our supported languages.

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Gets locale from a request
+"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from config import Config
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Gets locale from a request
+    """
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """
+    The index page route, renders the '2-index.html' template
+    """
+
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

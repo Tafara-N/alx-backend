c1e34da8c86664fbb242706ba44862823e763751
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-30 18:07:25 +0200

    A Config class that has a LANGUAGES class attribute.

---
+++ 0x02-i18n/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Babel config model
+"""
+
+class Config(object):
+    """
+    Configuration class for Babel
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"

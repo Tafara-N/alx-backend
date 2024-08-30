5370f48ada41db76f7a21d7444d79affec5df953
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-30 18:03:02 +0200

    Node Redis Client.

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+// Node Redis Client
+
+import { createClient } from 'redis';
+
+client = createClient();
+async function nodeRedis() {
+  try {
+    await client.connect()
+    console.log('Redis client connected to the server');
+  } catch (error) {
+    console.log('Redis client not connected to the server:', error);
+  }
+}
+nodeRedis()

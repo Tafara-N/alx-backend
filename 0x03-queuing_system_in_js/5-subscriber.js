f60e65a657fba58bc39de394c1835f54bf1e2b66
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-30 18:17:51 +0200

    Node Redis client subscriber.

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -6,19 +6,19 @@ const client = createClient();
 const EXIT_MSG = 'KILL_SERVER';
 
 client.on('error', (err) => {
-	console.log('Redis client not connected to the server:', err.toString());
+  console.log('Redis client not connected to the server:', err.toString());
 });
 
 client.on('connect', () => {
-	console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 client.subscribe('holberton school channel');
 
 client.on('message', (_err, msg) => {
-	console.log(msg);
-	if (msg === EXIT_MSG) {
-		client.unsubscribe();
-		client.quit();
-	}
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
 });

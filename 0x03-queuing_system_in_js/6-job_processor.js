f5394b86ae68cab3aed5a9f2bd6b5374ab30c725
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-30 18:23:10 +0200

    Job processor.

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,18 @@
+// Job processor
+
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber},`,
+    'with message:',
+    message,
+  );
+};
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});

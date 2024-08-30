466f5940a5b9bc2b3deb413415d7714c897dda7b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-30 18:21:21 +0200

    Job creator.

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+// Job creator
+
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '07055979239',
+  message: 'Account registered',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+
+job.save();
